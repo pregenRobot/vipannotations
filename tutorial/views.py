2858f969d0e8af3ec8689b8f77bbc9c337bf5d04
Tamim Azmain <maat1@st-andrews.ac.uk>
2021-03-17 15:57:41 +0900

    removed tutorial app

--- tutorial/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
-from datetime import datetime, timedelta
-from dateutil import tz, parser
-
-
-def home(request):
-	return HttpResponse("Welcome to the tutorial.")
