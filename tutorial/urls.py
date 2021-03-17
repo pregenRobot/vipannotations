d4ca7dab2c6a6a22143692557d788d8a6e375ae6
Tamim Azmain <maat1@st-andrews.ac.uk>
2021-03-17 15:55:37 +0900

    wip tutorial

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("",views.home, name = "home"),
-	path("signin",views.home, name="signin"),
-	path("calendar",views.home, name="calendar")
+	path("",views.home, name="home"),
+	path("signin",views.sing_in, name = "signin"),
+	path("signout",views.sign_out, name = "singout"),
+	path("calendar", views.home, name = "calendar"),
+	path("callback",views.callback, name="callback")
 ]
--- tutorial/views.py
+++ tutorial/views.py
@@ -1,9 +1,50 @@
+from typing import Dict
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from datetime import datetime, timedelta
 from dateutil import tz, parser
+from tutorial.auth_helper import *
+from tutorial.graph_helper import *
 
+def initialize_context(request) -> Dict:
+	context = {}
+
+	# Check fo any erros in the session
+	error = request.session.pop("flash_error",None)
+
+	if error!=None:
+		context["errors"] = []
+		context["errors"].append(error)
+
+	context["user"] = request.session.get("user",{"is_authenticated":False})
+	return context
+		
 
 def home(request):
-	return HttpResponse("Welcome to the tutorial.")
+	context = initialize_context(request)
+	return render(request,"tutorial/home.html",context)
+
+def sing_in(request):
+	flow = get_sign_in_flow()
+	try:
+		request.session["auth_flow"] = flow
+	except Exception as e:
+		print(e)
+	
+	return HttpResponseRedirect(flow["auth_uri"])
+
+def callback(request):
+	result = get_token_from_code(request)
+
+	user = get_user(result["access_token"])
+
+	request.session['flash_error'] = { 'message': 'Token retrieved', 'debug': 'User: {0}\nToken: {1}'.format(user, result) }
+
+	store_user(request,user)
+	return HttpResponseRedirect(reverse(home))
+
+def sign_out(request):
+	remove_user_and_token(request)
+
+	return HttpResponseRedirect(reverse("home"))
