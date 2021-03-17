b63455bbdebcd29c109104e8bf95fd5c6e7d164d
Tamim Azmain <maat1@st-andrews.ac.uk>
2021-03-17 12:41:51 +0900

    created view link

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from datetime import datetime, timedelta
+from dateutil import tz, parser
 
-# Create your views here.
+
+def home(request):
+	return HttpResponse("Welcome to the tutorial.")
---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from tutorial import views
+
+urlpatterns = [
+	path("",include("tutorial.urls")),
+	path("admin/",admin.site.urls)
+]
